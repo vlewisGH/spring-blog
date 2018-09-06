325fb0991110359fbd3970b99e7a16bfad07faec
vlewisGH <vincent.sean.lewis@gmail.com>
2018-09-05 19:02:00 -0500

    end of day commit

---
+++ src/main/java/com/blog/blog/MathController.java
@@ -0,0 +1,19 @@
+package com.blog.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class MathController {
+
+    @GetMapping("/add/{firstNumber}/and/{secondNumber}")
+    @ResponseBody
+    public int add(@PathVariable int firstNumber, @PathVariable int secondNumber){
+        return (firstNumber + secondNumber);
+    }
+
+}
